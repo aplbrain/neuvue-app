30aaae25b9968c9682cfd503a67357eb62796f25
Daniel Xenes <daniel.xenes@jhuapl.edu>
2021-10-06 14:34:12 -0400

    cleans up directory

--- neuvue/logout/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.views.generic.base import View
-
-
-class LogoutView(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, "logout.html")
