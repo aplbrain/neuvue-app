cefd88e5de6c3c91152b7851c0cb0a8e13cb43b8
ramsddc1 <Devin.Ramsden@jhuapl.edu>
2021-10-16 08:59:47 -0400

    getting ng links

--- neuvue/workspace/views.py
+++ neuvue/workspace/views.py
@@ -5,51 +5,74 @@ from django.conf import settings
 import pandas as pd
 import os
 
+import pickle
+import colocarpy
+import random
+from tqdm.notebook import tqdm
+from caveclient import CAVEclient
+from nglui.statebuilder import *
+import numpy as np
+
+def get_NG_link(coordinates):
+    img_source = 'precomputed://https://storage.googleapis.com/iarpa_microns/minnie/minnie65/em'
+    seg_source = 'graphene://https://minnie.microns-daf.com/segmentation/table/minnie3_v1'
+    img_layer = ImageLayerConfig(name='em',
+                                 source=img_source,
+                                 )
+    seg_layer = SegmentationLayerConfig(name = 'seg',
+                                        source = seg_source,
+                                       selected_ids_column='pt_root_id')
+    anno_layer = AnnotationLayerConfig(name='annos')
+    view_options = {'position': coordinates, 'zoom_image': 20}
+    sb = StateBuilder(layers=[img_layer, seg_layer, anno_layer], view_kws=view_options)
+    link = sb.render_state(url_prefix='http://neuroglancer.neuvue.io.s3-website-us-east-1.amazonaws.com')
+    return link
+
 class WorkspaceView(View):
 
+    def dispatch(self, request, *args, **kwargs):
+        self.client = colocarpy.Colocard(settings.COLOCARPY_ADDR)
+        return super().dispatch(request, *args, **kwargs)
 
     def get(self, request, *args, **kwargs):
-        return render(request, "workspace.html")
+        #restart begins as start
+        #if open display task
+        #this part not done
+        link = "http://neuroglancer.neuvue.io.s3-website-us-east-1.amazonaws.com/?local_id=6cbc0542207c4996743f4e12bd20f35d#"
+        args = {
+            'new_link': link
+        }
+        return render(request, "workspace.html", args)
 
     def post(self, request, *args, **kwargs):
 
-        df = pd.read_pickle(os.path.join(settings.STATICFILES_DIRS[1], "two_soma_df_private_data_10_05_2021.pkl"))
-
-
-        try:
-            client = colocarpy.Colocard("http://3.92.233.204:9005")
-        except Exception as e:
-            print("could not connect")
-        
-        # Map point by their root ID, 
-        point_map = create_point_map(df.iloc[:50])
-
-        # Post points and save the IDs of each posted point to reference to a task
-        id_map = post_points_from_map(client, point_map)
-
-        # Post tasks from the id map
-        posted_tasks = post_tasks_from_map(client, id_map)
-
         if 'restart' in request.POST:
-            #client.get_task() need a get current task
-            #Gets the coordinates of the current task. Sets neuroglancer state to go to those coordinates.
-            client.get_current_task('andy', 'neuvue')
-            #get_points
-            #feed to neuroglancer and gen link
-            #pass link into context
-            #show on iframe
             print("restart")
+            task = self.client.get_next_task("andy", "neuvue")
+            point = self.client.get_point(task['points'][0])
+            coordinates = np.array(point['coordinate'])
+            link = get_NG_link(coordinates)
+
         if 'next' in request.POST:
-            #this will submit change status of curr task - clear workspace - display new button that loads next task, change to open, gen link and dispaly
-            client.get_next_task('andy', 'neuvue')
-            #Updates the current task status from "open" to "closed". Gets the next task. Sets neuroglancer state to go to those coordinates.
             print("next")
+            task = self.client.get_next_task("andy", "neuvue")
+            self.client.patch_task(task["_id"], status = "complete")
+            point = self.client.get_point(task['points'][0])
+            coordinates = np.array(point['coordinate'])
+            link = get_NG_link(coordinates)
         if 'flag' in request.POST:
-            #Updates the current task status from "open" to "errored".  Updates the current task metadata with new key:value containing "flag-reason": `message`. Gets the next task (NextTaskView)
             print("flag")
+            task = self.client.get_next_task("andy", "neuvue")
+            self.client.patch_task(task["_id"], status = "errored")
+            point = self.client.get_point(task['points'][0])
+            coordinates = np.array(point['coordinate'])
+            link = get_NG_link(coordinates)
 
-        return render(request, "workspace.html")
+        args = {
+            'new_link': link
+        }
 
+        return render(request, "workspace.html", args)
 
 
 class TaskView(View):
