1eefdcf14c70c0c9ee30fe16686d3d10dc799408
Tori Rose <46499982+ttorir@users.noreply.github.com>
2021-10-19 16:11:05 -0400

    Layout adjustments 10 14 (#20)
    * proofreading page changes + base: navbar reorganization
    
    * tabs moved on task page
    
    * comments added
    
    * eventListener -> onClick for some functions
    
    * remaining formatting issues for issues #16 & #17
    
    Co-authored-by: Rose, Victoria A <victoria.rose@jhuapl.edu>
    Co-authored-by: Daniel Xenes <daniel.xenes@jhuapl.edu>

--- django/static/js/utils.js
+++ django/static/js/utils.js
@@ -1,10 +1,11 @@
+/* Toggles the Small Drop Down Menus in the Task Queue List */
 function block_toggle() {
 
   var acc = document.getElementsByClassName("taskButton");
   var i;
 
   for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
+    acc[i].onclick=function() {
       /* Toggle between adding and removing the "active" class,
       to highlight the button that controls the panel */
       this.classList.toggle("active");
@@ -16,21 +17,19 @@ function block_toggle() {
       } else {
         panel.style.display = "block";
       }
-    });
+    };
   }
 }
 
 /* This controls information from appearing in the sidebar*/
-var side_button = document.getElementById("sidebarActivate");
-  side_button.onclick=function sidemenu_content() {
-    
-    var i_container = document.getElementById("instruction-container");
-    var q_container = document.getElementById("queue-container");
+
+function sidemenu_content() {
+    var side_button = document.getElementById("sidebarActivate");
     var neuroglancer_window = document.getElementById("neuroglancer");
 
     /* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel */
-    this.classList.toggle("active");
+    side_button.classList.toggle("active");
 
     
       if (neuroglancer_window.style.width != "75%" ) {
@@ -39,9 +38,9 @@ var side_button = document.getElementById("sidebarActivate");
         closeSideMenu()
       }
 
-    } 
+} 
 
-
+/* Opens Side Menu */
 function openSideMenu(){
   var sidemenu = document.getElementById("neuVue-sidemenu");
   var sidebar = document.getElementById("neuVue-sidebar");
@@ -50,18 +49,23 @@ function openSideMenu(){
 
   sidemenu.style.width = "25%";
   sidebar.style.width = "8%";
-  sidecontent.style.width = "80%";
+  sidecontent.style.width = "85%";
   sidecontent.style.visibility = "visible";
+
+  sideBoxes = document.getElementsByClassName("instructionBox");
+  for (i = 0; i < sideBoxes.length; i++) {
+      sideBoxes[i].style.visibility = "visible";
+  }
 
   sidemenu.style.transition = "0.3s";
   sidebar.style.transition = "0.15s";
   sidecontent.style.animationDelay = "0.29s";
 
-  
   neuroglancer_window.style.width = "75%";
+  neuroglancer_window.style.left = "0%";
 
 }
-
+/* Closes Side Menu */
 function closeSideMenu() {
   var sidemenu = document.getElementById("neuVue-sidemenu");
   var sidebar = document.getElementById("neuVue-sidebar");
@@ -73,37 +77,24 @@ function closeSideMenu() {
   sidecontent.style.width = "0%";
   sidecontent.style.visibility = "hidden";
 
+  sideBoxes = document.getElementsByClassName("instructionBox");
+  for (i = 0; i < sideBoxes.length; i++) {
+      sideBoxes[i].style.visibility = "hidden";
+  }
+
   sidemenu.style.transition = "0.3s";
   sidebar.style.transitionDelay = "0.15s";
   sidecontent.style.transition = "0.3s";
 
   
-  neuroglancer_window.style.width = "97%";
+  neuroglancer_window.style.width = "101%";
+  neuroglancer_window.style.left = "-2%";
 }
 
-/* FOR TAKS PAGE */
 
-function table_toggle() {
 
-  var acc = document.getElementsByClassName("jobHeader");
-  var i;
 
-  for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-      /* Toggle between adding and removing the "active" class,
-      to highlight the button that controls the panel */
-      this.classList.toggle("active");
 
-      /* Toggle between hiding and showing the active panel */
-      var panel = this.nextElementSibling;
-      if (panel.style.display == "block") {
-        panel.style.display = "none";
-      } else {
-        panel.style.display = "block";
-      }
-    });
-  }
-}
 
 
 
