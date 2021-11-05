19ddc415b0d53ff683daba6de8d8d34e234dda81
Daniel Xenes <daniel.xenes@jhuapl.edu>
2021-11-05 01:44:58 -0400

    fixes import

--- django/neuvue/wsgi.py
+++ django/neuvue/wsgi.py
@@ -8,8 +8,12 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if BASE_DIR not in sys.path:
+    sys.path.insert(0, BASE_DIR)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'neuvue.settings')
 
