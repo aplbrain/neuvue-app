63148c9061e37df408c955250d980e6c2c14a400
Daniel Xenes <daniel.xenes@jhuapl.edu>
2021-11-09 12:13:14 -0500

    rebased changes

--- neuvue_project/workspace/static/js/utils.js
+++ neuvue_project/workspace/static/js/utils.js
@@ -24,19 +24,21 @@ function block_toggle() {
 /* This controls information from appearing in the sidebar*/
 
 function sidemenu_content() {
-    var side_button = document.getElementById("sidebarActivate");
-    var neuroglancer_window = document.getElementById("neuroglancer");
+  console.log('side menu content clicked')
+  var side_button = document.getElementById("sidebarActivate");
+  var neuroglancer_window = document.getElementById("neuroglancer");
 
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    side_button.classList.toggle("active");
+  /* Toggle between adding and removing the "active" class,
+  to highlight the button that controls the panel */
+  side_button.classList.toggle("active");
 
-    
-      if (neuroglancer_window.style.width != "75%" ) {
-        openSideMenu()
-      } else {
-        closeSideMenu()
-      }
+    if (neuroglancer_window.style.width != "75%" ) {
+      openSideMenu();
+      updateSideBar(1);
+    } else {
+      closeSideMenu();
+      updateSideBar(0);
+    }
 
 } 
 
@@ -63,6 +65,7 @@ function openSideMenu(){
 
   neuroglancer_window.style.width = "75%";
   neuroglancer_window.style.left = "0%";
+  
 }
 /* Closes Side Menu */
 function closeSideMenu() {
@@ -88,10 +91,47 @@ function closeSideMenu() {
   
   neuroglancer_window.style.width = "101%";
   neuroglancer_window.style.left = "-2%";
-  
 }
+function getCookie(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+      const cookies = document.cookie.split(';');
+      for (let i = 0; i < cookies.length; i++) {
+          const cookie = cookies[i].trim();
+          // Does this cookie string begin with the name we want?
+          if (cookie.substring(0, name.length + 1) === (name + '=')) {
+              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+              break;
+          }
+      }
+  }
+  return cookieValue;
+}
+function updateSideBar(action) {
+  
+  const csrftoken = getCookie('csrftoken');
 
-
+  const myInit = {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json, text/plain, */*',
+          'X-CSRFToken': csrftoken,
+      },
+      body: JSON.stringify({'sidebar_tab': action}),
+      credentials: 'same-origin',
+  };
+  fetch('', myInit).then(function (response) {
+      /*
+      if (response.ok) {
+          console.log('success')
+      }
+      else {
+          console.log('failed')
+      }
+      */
+  });
+}
 
 
 
