eea431016a67080303e283d09ef15b70ccc2a449
Rose, Victoria A <vrose9@jhu.edu>
2021-11-22 14:38:51 -0500

    model update

--- neuvue_project/workspace/models.py
+++ neuvue_project/workspace/models.py
@@ -25,10 +25,21 @@ class Namespace(models.Model):
         ('submit', 'Submit Button'),
         ('forced_choice', 'Yes/No/Maybe Button'),
     ]
+    pcg_choices = [
+        ('Minnie', 'https://minnie.microns-daf.com/segmentation/table/minnie3_v1'),
+        ('Pinky', 'https://global.daf-apis.com/info/datastack/pinky_sandbox'),
+    ]
+    img_choices = [
+        ('Minnie', 'https://bossdb-open-data.s3.amazonaws.com/iarpa_microns/minnie/minnie65/em'),
+        ('Pinky', 'gs://microns_public_datasets/pinky100_v0/son_of_alignment_v15_rechunked'),
+    ]
+
     namespace = models.CharField(max_length=50, primary_key=True)
     display_name = models.CharField(max_length=100)
     ng_link_type = models.CharField(max_length=50, choices = NeuroglancerLinkType.choices, default= NeuroglancerLinkType.POINT)
     submission_method = models.CharField(max_length=50, choices=submission_method_choices, default="submit")
+    PCG_SOURCE = models.CharField(max_length=300, choices=pcg_choices, default='Minnie')
+    IMG_SOURCE = models.CharField(max_length=300, choices=img_choices, default='Minnie')
     
     def __str__(self):
         """String for representing the MyModelName object (in Admin site etc.)."""
