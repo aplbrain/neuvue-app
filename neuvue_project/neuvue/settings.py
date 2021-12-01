d94f61f5f1e7cf0aca798ab6743a7ac62f218ad5
ramsddc1 <Devin.Ramsden@jhuapl.edu>
2021-12-01 10:53:28 -0500

    update settings

--- neuvue_project/neuvue/settings.py
+++ neuvue_project/neuvue/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-x&k71)cwa@+a_0eg0sewzjwdyh!rzcy+$)c_e!f*-leem==lcf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'app.neuvue.io',
