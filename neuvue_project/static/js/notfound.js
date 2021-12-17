0110845522f4e24de92c8578ede8547583e22607
Daniel Xenes <daniel.xenes@jhuapl.edu>
2021-12-17 00:19:50 -0500

    remove static dir

--- neuvue_project/static/js/notfound.js
+++
@@ -1,2 +0,0 @@
-//import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js';
-//import * as GLTFLoader from 'https://unpkg.com/three@0.135.0/examples/js/loaders/GLTFLoader.js';
