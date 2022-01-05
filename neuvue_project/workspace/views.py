f2ac57c63f834202290fe80a91c5f4a6b6cbb6c3
Diego Luna <50876892+diegoluna3@users.noreply.github.com>
2022-01-05 10:06:54 -0800

    Intertask page (#93)
    * add logic to bypass intertask page by patching next task as open
    
    * remove comments
    
    * change logic to pending task status
    
    Co-authored-by: Daniel Xenes <daniel.xenes@jhuapl.edu>

--- neuvue_project/workspace/views.py
+++ neuvue_project/workspace/views.py
@@ -60,8 +60,11 @@ class WorkspaceView(LoginRequiredMixin, View):
             context['tasks_available'] = False
             pass
 
-        elif task_df['status'] == 'open':
+        else:
 
+            if task_df['status'] == 'pending':
+                self.client.patch_task(task_df["_id"], status="open")
+            
             # Update Context
             context['is_open'] = True
             context['task_id'] = task_df['_id']
