3fae42d8b22e0ce9382247068904176de2002be6
Daniel Xenes <daniel.xenes@jhuapl.edu>
2022-04-06 15:01:22 -0400

    separates tags

--- neuvue_project/dashboard/templatetags/inclusion_tags.py
+++ neuvue_project/dashboard/templatetags/inclusion_tags.py
@@ -3,7 +3,7 @@ from django import template
 register = template.Library()
 
 @register.inclusion_tag('reusable_components/confirm_modal.html')
-def show_modal(column, input_value):
+def create_modal(column):
     # Update syntax when you implement for delete
     return {
         "column": column.lower(),
@@ -11,5 +11,13 @@ def show_modal(column, input_value):
         "confirm_text_id": "confirmUpdate" + column.capitalize() + "ModalBody",
         "button_id": "update_" + column.lower() + "_modal_button",
         "button_value": column.lower(),
-        "input_value": input_value
+    }
+
+@register.inclusion_tag('reusable_components/trigger_modal.html')
+def trigger_modal(column):
+    return {
+        "column": column.lower(),
+        "modal_id": "confirmUpdate" + column.capitalize() + "Modal",
+        "confirm_text_id": "confirmUpdate" + column.capitalize() + "ModalBody",
+        "button_id": column.lower() + "_button",
     }
