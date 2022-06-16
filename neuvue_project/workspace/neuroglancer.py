8d25d7de279ae3cdb63db5a27b0bfca32e14d6e2
RachelBrodsky <67277602+RachelBrodsky@users.noreply.github.com>
2022-06-16 13:41:11 -0400

    Replace palettes with 10 color palettes and remove color looping
    Currently this uses the same palettes for annotation and mesh color palette options, we can easily change this if we want to.

--- neuvue_project/workspace/neuroglancer.py
+++ neuvue_project/workspace/neuroglancer.py
@@ -424,22 +424,29 @@ def apply_state_config(state:str, username:str):
         cdict["navigation"]["zoomFactor"] = int(zoom_level)
     
     # create color palette dictionary
-    annotation_color_palette_dict = {'palette1' : ['#F9C80E', '#F86624', '#EA3546', '#662E9B', '#43BCCD'],
-                            'palette2' : ['#006D77', '#83C5BE', '#EFG6F9', '#FFDDD2', '#E29578'], 
-                            'palette3' : ['#22577A', '#38A3A5', '#57CC99', '#80ED99', '#C7F9CC'], 
-                            'palette4' : ['#25CED1', '#FFFFFF', '#FCEADE', '#FF8A5B', '#EA526F'], 
-                            'palette5' : ['#335C67', '#FFF3B0', '#E09F3E', '#9E2A2B', '#540B0E'], 
-                            'palette6' : ['#FF99C8', '#FCF6BD', '#D0F4DE', '#A9DEF9', '#E4C1F9']}
+    annotation_color_palette_dict = {'palette1' : ['#FFADAD', '#FFD6A5', '#FDFFB6', '#CAFFBF', '#9BF6FF', '#A0C4FF', '#BDB2FF', '#FFC6FF', '#FFFFFC'],
+                            'palette2' : ['#F72585', '#B5179E', '#7209B7', '#560BAD', '#480CA8', '#3A0CA3', '#3F37C9', '#4361EE', '#4895EF', '#4CC9F0'], 
+                            'palette3' : ['#7400B8', '#6930C3', '#5E60CE', '#5390D9', '#4EA8DE', '#48BFE3', '#56CFE1', '#64DFDF', '#72EFDD', '#80FFDB'], 
+                            'palette4' : ['#F94144', '#F3722C', '#F8961E', '#F9844A', '#F9C74F', '#90BE6D', '#43AA8B', '#4D908E', '#577590', '#277DA1'], 
+                            'palette5' : ['#005F73', '#0A9396', '#94D2BD', '#E9D8A6', '#EE9B00', '#CA6702', '#BB3E03', '#AE2012', '#9B2226'], 
+                            'palette6' : ['#011A51', '#1957DB', '#487BEA', '#7EA3F1', '#C8D7F9', '#B83700', '#F06C00', '#FAB129', '#FBC55F', '#FDE9C3']}
 
     # create mesh color palette dictionary
-    mesh_color_palette_dict = {'palette1' : ['#F9C80E', '#F86624', '#EA3546', '#662E9B', '#43BCCD'],
-                        'palette2' : ['#006D77', '#83C5BE', '#EFG6F9', '#FFDDD2', '#E29578'], 
-                        'palette3' : ['#22577A', '#38A3A5', '#57CC99', '#80ED99', '#C7F9CC'], 
-                        'palette4' : ['#25CED1', '#FFFFFF', '#FCEADE', '#FF8A5B', '#EA526F'], 
-                        'palette5' : ['#335C67', '#FFF3B0', '#E09F3E', '#9E2A2B', '#540B0E'], 
-                        'palette6' : ['#FF99C8', '#FCF6BD', '#D0F4DE', '#A9DEF9', '#E4C1F9']}
+    mesh_color_palette_dict = {'palette1' : ['#FFADAD', '#FFD6A5', '#FDFFB6', '#CAFFBF', '#9BF6FF', '#A0C4FF', '#BDB2FF', '#FFC6FF', '#FFFFFC'],
+                            'palette2' : ['#F72585', '#B5179E', '#7209B7', '#560BAD', '#480CA8', '#3A0CA3', '#3F37C9', '#4361EE', '#4895EF', '#4CC9F0'], 
+                            'palette3' : ['#7400B8', '#6930C3', '#5E60CE', '#5390D9', '#4EA8DE', '#48BFE3', '#56CFE1', '#64DFDF', '#72EFDD', '#80FFDB'], 
+                            'palette4' : ['#F94144', '#F3722C', '#F8961E', '#F9844A', '#F9C74F', '#90BE6D', '#43AA8B', '#4D908E', '#577590', '#277DA1'], 
+                            'palette5' : ['#005F73', '#0A9396', '#94D2BD', '#E9D8A6', '#EE9B00', '#CA6702', '#BB3E03', '#AE2012', '#9B2226'], 
+                            'palette6' : ['#011A51', '#1957DB', '#487BEA', '#7EA3F1', '#C8D7F9', '#B83700', '#F06C00', '#FAB129', '#FBC55F', '#FDE9C3']}
 
 
+
+    # generate random color
+    def getRandomHexColor():
+        random_number = random.randint(0,16777215)
+        hex_number = str(hex(random_number))
+        hex_color = '#'+ hex_number[2:].upper()
+        return hex_color
     
     annotation_layer_count = 0
     for layer in cdict['layers']:
@@ -453,7 +460,12 @@ def apply_state_config(state:str, username:str):
         # handle annotation layer colors
         if layer.get('type', '') == 'annotation' and config.annotation_color_palette_switch:
             annotation_color_palette_list = annotation_color_palette_dict[annotation_color_palette]
-            annotation_color = annotation_color_palette_list[annotation_layer_count%len(annotation_color_palette_list)]
+            
+            # set annotation color 
+            # get a random color, if the layer is within the number of available color palette colors, grab next color paltte color
+            annotation_color = getRandomHexColor()  # set this to random color
+            if annotation_layer_count < len(annotation_color_palette_list):
+                annotation_color = annotation_color_palette_list[annotation_layer_count%len(annotation_color_palette_list)]
             layer['annotationColor'] = str(annotation_color)
             annotation_layer_count += 1
     
@@ -464,9 +476,12 @@ def apply_state_config(state:str, username:str):
             segmentColors = {}
 
             # populate segment colors dictionary
+            # get a random color, if the layer is within the number of available color palette colors, grab next color paltte color
             mesh_layer_count = 0
             for segment in segments:
-                mesh_color = mesh_color_palette_list[mesh_layer_count%len(mesh_color_palette_list)]
+                mesh_color = getRandomHexColor()
+                if mesh_layer_count < len(mesh_color_palette_list):
+                    mesh_color = mesh_color_palette_list[mesh_layer_count%len(mesh_color_palette_list)]
                 segmentColors[segment] = mesh_color
                 mesh_layer_count += 1
             
