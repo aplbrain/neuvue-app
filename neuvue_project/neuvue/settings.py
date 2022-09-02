d9d3ded833e978d8dacc491f4d0822af84391789
lunada1 <dluna3@jhu.edu>
2022-09-02 14:44:33 -0400

    #339 set secret key to env variable

--- neuvue_project/neuvue/settings.py
+++ neuvue_project/neuvue/settings.py
@@ -18,8 +18,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ['SECRET_KEY']
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'app.neuvue.io',
