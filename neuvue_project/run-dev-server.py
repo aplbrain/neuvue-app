b97f45ebf7ad482f915d347190300b0ddb8ffe60
Daniel Xenes <daniel.xenes@jhuapl.edu>
2023-01-09 14:02:30 -0500

    updates run dev server to be win32 compat

--- neuvue_project/run-dev-server.py
+++ neuvue_project/run-dev-server.py
@@ -1,4 +1,7 @@
+#!/usr/bin/python3
+
 import subprocess
+from sys import platform
 
 # Turn debug mode on in settings.py
 with open("neuvue/settings.py", "r") as f:
@@ -8,11 +11,21 @@ with open("neuvue/settings.py", "r") as f:
 with open("neuvue/settings.py", "w") as f:
     f.write(settings)
 
-# Get recent migrations to database
-subprocess.run(["python3", "manage.py", "migrate"], shell=True)
+if platform == "win32":
+    # Get recent migrations to database
+    subprocess.run(["python", "manage.py", "migrate"], shell=True)
 
-# Collect static files
-subprocess.run(["python3", "manage.py", "collectstatic", "--no-input"], shell=True)
+    # Collect static files
+    subprocess.run(["python", "manage.py", "collectstatic", "--no-input"], shell=True)
 
-# Run Dev server on localhost
-subprocess.run(["python3", "manage.py", "runserver", "localhost:8000"], shell=True)
+    # Run Dev server on localhost
+    subprocess.run(["python", "manage.py", "runserver", "localhost:8000"], shell=True)
+else:
+    # Get recent migrations to database
+    subprocess.run(["python3", "manage.py", "migrate"])
+
+    # Collect static files
+    subprocess.run(["python3", "manage.py", "collectstatic", "--no-input"])
+
+    # Run Dev server on localhost
+    subprocess.run(["python3", "manage.py", "runserver", "localhost:8000"])
