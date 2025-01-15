57b3c19680d3c7cfa9f3dde78ff096bfbc388bfb
Daniel Xenes <daniel.xenes@jhuapl.edu>
2025-01-15 15:45:37 -0500

    django model changes

--- neuvue_project/neuvue/settings.py
+++ neuvue_project/neuvue/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 SITE_ID = 2
 
